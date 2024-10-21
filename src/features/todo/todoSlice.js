9c43a7a149929c7e213a8824f4dced9d74ef41c4
Anas Abbas <anasabbasmalik88@gmail.com>
2024-10-21 15:51:19 +0500

    craeting todoApp with Redux-Toolkit

---
+++ src/features/todo/todoSlice.js
@@ -0,0 +1,33 @@
+import { createSlice , nanoid} from "@reduxjs/toolkit";
+
+const initialState = {
+    todos : [{id : 1 , text : "Hello Redux!"}]
+}
+
+export const todoSlice = createSlice({
+    name : "todo",
+    initialState : initialState,
+    reducers : {
+        addTodo : (state , action) => {
+            const todo = {
+                id : nanoid(),
+                text : action.payload
+            }
+            state.todos.push(todo);
+        },
+        removeTodo : (state , action) => {
+            state.todos = state.todos.filter((todo)=> todo.id !== action.payload)
+        },
+
+        updateTodo : (state , action) =>{
+            const index = state.todos.findIndex((reqTodo) => reqTodo.id == action.payload.id);
+            if (index !== -1){
+                state.todos[index].text = action.payload.text;
+            }
+        }
+    }
+})
+
+ export const {addTodo , removeTodo , updateTodo} = todoSlice.actions;
+
+ export const todoReducer = todoSlice.reducer;
